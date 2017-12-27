b17ef44e3dc4e7e836ea65ce241717d73870258b
Rishabh Sharma <rishabhsharma@ufl.edu>
2017-12-27 10:53:40 -0500

    feat(module-api): initialize VehicleController with basic definations.

---
+++ api/CarTracker/src/main/java/risshar/controller/VehicleController.java
@@ -0,0 +1,36 @@
+package risshar.controller;
+
+import org.springframework.stereotype.Controller;
+import risshar.entity.Vehicle;
+
+import java.util.List;
+
+@Controller
+public class VehicleController {
+
+    public List<Vehicle> findAll()
+    {
+        return null;
+    }
+
+    public Vehicle findOne()
+    {
+        return null;
+    }
+
+    public Vehicle create()
+    {
+        return null;
+    }
+
+    public List<Vehicle> update()
+    {
+        return null;
+    }
+
+    public void delete()
+    {
+        return;
+    }
+}
+
